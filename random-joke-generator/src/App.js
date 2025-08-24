ff4466503e21b6d477b75efdbec8031bbe318d94
buyath <buyaththayub@gmail.com>
2025-08-24 22:46:24 +0000

    feat : i have done the joke generator app

--- random-joke-generator/src/App.js
+++ random-joke-generator/src/App.js
@@ -1,24 +1,24 @@
-import React,{useState,useEffect}  from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
-// import CreateJoke from "./components/joke.js";
-
-const JokeApp =  () => {
-    const [joke,setJoke] = useState(null);
-    useEffect(()  => {
-        const fetchJoke =async () => {
-            const data = await fetch("https://official-joke-api.appspot.com/random_joke");
-            const result = await data.json();
-            return result;   
-        }
-    },[]);
-    return(
-        <div>
-            <h1>Joke Generator</h1>
-            {console.log(joke.Object.id)}
-            {/* <CreateJoke id  = {joke.id} setup = {joke.setup} punchline={joke.punchline}/> */}
+const JokeApp = () => {
+    const [joke, setJoke] = useState("");
+    async function fetchData() {
+        const data = await fetch("https://official-joke-api.appspot.com/random_joke");
+        const response = await data.json();
+        setJoke(response);
+    } 
+    return (
+        <div className="main">
+            <div className="joke-container">
+                <h4 className = "joke-id">Joke id :{joke.id}</h4>
+                <h2>{joke.setup}</h2>
+               <div className="btn-container">
+                    <button className="gen-joke" onClick={fetchData}>getJoke</button>
+               </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<JokeApp/>)
+root.render(<JokeApp />);
--- random-joke-generator/src/test.js
+++ random-joke-generator/src/test.js
@@ -1,26 +1,2 @@
-import React,{useState,useEffect} from "react";
-import ReactDOM from "react-dom/client";
-
-const Test = () => {
-    const [btn,setbtn] = useState("Login");
-    useEffect(() => {
-        console.log("UseEffect Called...")
-    },[])
-    return (
-        <div>
-            <button onClick={() => {
-                 if(btn == "Login"){
-                    setbtn("Logout");
-                 }
-                 else{
-                    setbtn("Login");
-                 }
-            }}>{btn}</button>
-            <h1>Test</h1>
-        </div>
-    )
-}
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Test/>)
+let joke  = "this is a joke";
+console.log(joke ? `joke is ${joke}` : "there is no error");
